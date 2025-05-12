e96e781b8412e844c1a3012890cfea8a17edaece
Ilia-Ivankov <ilaivankov919@gmail.com>
2025-05-12 19:43:42 +0300

    исрпавление ошибок

--- task_manager/tests/settings_test.py
+++ task_manager/tests/settings_test.py
@@ -1,17 +1,29 @@
-from task_manager.settings import *
+"""
+Test settings for Django tests
+"""
+
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 SECRET_KEY = 'django-insecure-test-key-for-running-tests-only'
 
 DEBUG = False
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': ':memory:',
     }
 }
+
 ROLLBAR = {
     'access_token': 'test_token',
     'environment': 'test',
     'code_version': '1.0',
     'root': BASE_DIR,
-} 
+}
+
+# Import all other settings from the main settings file
+# This should be at the end of the file so that overrides above take precedence
+from task_manager.settings import *  # noqa F403 
