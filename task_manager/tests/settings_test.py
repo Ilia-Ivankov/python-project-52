60f19f45423c98645876ecdcebc7c73d7603fd1d
Ilia-Ivankov <ilaivankov919@gmail.com>
2025-05-12 19:47:50 +0300

    исправление ошибок

--- task_manager/tests/settings_test.py
+++ task_manager/tests/settings_test.py
@@ -3,11 +3,15 @@ Test settings for Django tests
 """
 
 from pathlib import Path
+from task_manager.settings import *  # noqa: F403
 
+# Define BASE_DIR similar to main settings
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
+# Fixed SECRET_KEY for tests
 SECRET_KEY = 'django-insecure-test-key-for-running-tests-only'
 
+# Disable DEBUG for tests
 DEBUG = False
 
 DATABASES = {
@@ -17,13 +21,10 @@ DATABASES = {
     }
 }
 
+# Disable Rollbar for tests
 ROLLBAR = {
     'access_token': 'test_token',
     'environment': 'test',
     'code_version': '1.0',
     'root': BASE_DIR,
-}
-
-# Import all other settings from the main settings file
-# This should be at the end of the file so that overrides above take precedence
-from task_manager.settings import *  # noqa F403 
+} 
